4fa54c239c091544e78562d74473e0bdc3835016
Ankur Saxena <46398844+ankur-saxena-88@users.noreply.github.com>
2024-08-09 15:35:17 +0530

    Add InterpolationDemo.kt

---
+++ Basic Programs/InterpolationDemo.kt
@@ -0,0 +1,12 @@
+//InterpolationDemo.kt
+//Mon, 05-08-2024
+
+//Kotlin String interpolation demo program
+
+fun main(){
+
+    val length = 5
+    val width = 3
+
+    println("The area of the rectangle is: ${length * width}")
+}
