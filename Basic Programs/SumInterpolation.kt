acb699ddaeb682f6998e3f8af09c51873ef72290
Ankur Saxena <46398844+ankur-saxena-88@users.noreply.github.com>
2024-08-09 15:40:31 +0530

    Add SumInterpolation.kt
    Kotlin program to calculate sum of two numbers using String interpolation method

---
+++ Basic Programs/SumInterpolation.kt
@@ -0,0 +1,17 @@
+//SumInterpolation.kt
+//Fri, 09-08-2024
+
+/*
+Kotlin program to find sum of two numbers using string interpolation method.
+*/
+
+fun main(){
+    val num1 = 20
+    val num2 = 15
+
+    println("First number is: $num1")
+    println("Second number is: $num2")
+
+    //calculate sum and print result
+    println("The sum of $num1 and $num2 is: ${num1 + num2}")
+}
