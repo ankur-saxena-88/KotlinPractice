1e354daee68fdb60ca0f09b8ee25c51e11977231
Ankur Saxena <46398844+ankur-saxena-88@users.noreply.github.com>
2024-08-09 15:32:34 +0530

    Add HelloKotlin.kt file

---
+++ HelloWorld Program/HelloKotlin.kt
@@ -0,0 +1,14 @@
+//HelloKotlin.kt
+//Sun, 04-05-2024
+
+/*
+Kotlin program that prints "Hello, Kotlin World!" on the console screen using argument and array of string method.
+*/
+
+fun main(args: Array<String>){
+
+    println("Hello, Kotlin World!")
+
+//    print("Hello, ")
+//    print("World!")
+}
